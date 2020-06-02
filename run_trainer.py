c80023a301086dc9cef2d7df59d59e94b39b402b
Tung <tst008@bucknell.edu>
2020-06-02 09:09:03 -0400

    Minor

---
+++ run_trainer.py
@@ -0,0 +1,14 @@
+from typing import Dict, Any
+from time import sleep
+
+import torch
+from botorch.test_functions.synthetic import Hartmann
+
+from src.synthetic_trainers import NegHartmannTrainer
+
+
+if __name__ == "__main__":
+    trainer = NegHartmannTrainer(host="jvs008-r1.bucknell.edu",
+                                 port="15555")
+    trainer.run()
+    
